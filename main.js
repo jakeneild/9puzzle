22b4ed801bfe3a65229a45d15ccb161e1f8dcf77
Jake Neild <jakeneildfb@yahoo.com>
2018-05-28 21:08:33 -0500

    Some minor changes.  Still not working correctly.

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ var domArray = []; //contains references to parent divs
 var domChildArray = []; //contains reference to child divs
 var positionArray = ["0px 0px", "-150px 0px", "-300px 0px", "0px -150px", "-150px -150px", "-300px -150px", "0px -300px", "-150px -300px", "empty"];  //contains background positions
 
-//check writing for new positionArray position
-
 //assign divs to variables
 for(let i = 0; i < idArray.length; i++){
     domArray[i] = document.getElementById(idArray[i]);
@@ -27,16 +25,35 @@ console.log(domChildArray);
 
 //check for win
 
-//click events
-function switchDivs(e){
+/*function checkWin(){  //untested
+    let win = false;
+    for(i=0; i < positionArray.length; i++{
+        if(domArray[i].firstChild.style.backgroundPosition != positionArray[i]{
+            win = false;
+        }
+    }
+    if(win= true){
+        var endPic = document.createElement("div");
+        endPic.style.width = 450px;
+        endPic.style.height = 450px;
+        endpic.style.backgroundImage = url('pic.jpg');
+        endpic.z-index = 2;  //never used this
+        document.querySelector("body").appendChild(endPic);
+    }
+}
+*/
+
+
+function switchDivs(e){  //this function doesn't work 100%
     let parentTarget = e.target;
     if(e.target.className != "empty"){  //need to add adjacent movement only 
-        console.log("childTargetID: "+  e.target.parentElement.firstChild.id);
+        //console.log("childTargetID: "+  e.target.parentElement.firstChild.id);
         let childTargetId = e.target.parentElement.firstChild.id;
-        console.log(childTargetId);
+        console.log("childTargetID: " + childTargetId);
         let childTarget = domChildArray[childTargetId];
-        console.log(childTarget);
+        console.log("childTarget: " + childTarget);
         let emptyDiv = document.getElementsByClassName("empty").item(0);
+
         let emptyDivIndex = domChildArray.indexOf("empty");
         
         emptyDiv.classList.remove("empty");
@@ -48,17 +65,18 @@ function switchDivs(e){
         newChildDiv.id = childTargetId;
         emptyDiv.appendChild(newChildDiv);
 
-        domChildArray[childTargetId] = newChildDiv;
-        domChildArray[emptyDivIndex] = "empty";  //just changed the target of this and the preceding line
+        domChildArray[childTargetId] = "empty";
+        domChildArray[emptyDivIndex] = newChildDiv;  //just changed the target of this and the preceding line
 
         e.target.parentElement.innerHTML = " "; 
 
-        positionArray[emptyDivIndex] = newChildDiv;
+        positionArray[emptyDivIndex] = positionArray[childTargetId];
         positionArray[childTargetId] = "empty";
 
-        //console.log(positionArray);
-        //console.log(domChildArray);
-        //console.log(domArray);
+        //checkWin();
+        console.log("position array: " + positionArray);
+        console.log("domChildArray: "+ domChildArray);
+        console.log("domArray" + domArray);
 
         //fix position array and dom array
     }
